fc632b44c8a73cc040671cde97f749350aa73798
Linglin Huang <lhuang@lupus.dfci.harvard.edu>
2021-12-05 23:55:09 -0500

    clean up code for clustering

--- 1_code/clustering/3_correlate_clusters.R
+++ 1_code/clustering/3_correlate_clusters.R
@@ -20,7 +20,7 @@ clustering_date <- "2020-03-25"
 
 treatment <- "EAE"
 if (treatment == "UT") {
-  tissue_vec <- c("SPL", "MLN", "PP", "SI", "COL")
+  tissue_vec <- c("SPL", "MLN", "PP", "SI", "COL") 
   dir_out <- paste0("2_pipeline/clustering/UT_GFPall_intra/", clustering_date, "/")
 } else if (treatment == "EAE") {
   tissue_vec <- c("SPL", "MLN", "PP", "SI", "COL", "CNS", "DLN")
@@ -60,28 +60,17 @@ M <- as.matrix(cluster_profile) %>% t
 sim <- M / sqrt(rowSums(M * M))
 cosine_sim <- sim %*% t(sim)
 
-pdf(paste0(dir_out, "cluster_similarity_", today, ".pdf"))
+## convert to distance and hierarchical clustering
 dist_mat <- as.dist(1 - cosine_sim)
 hc <- hclust(dist_mat, method = "ward.D")
-plot(hc)
-
 hc_order = hc$order
 dist_mat_transformed <- cosine_sim[hc_order, hc_order]
 diag(dist_mat_transformed) <- NA
 
-## correlation (cosine distance) plot
-corrplot(dist_mat_transformed, type="upper", order="original", method = "circle",
-         col=rev(brewer.pal(n=8, name="RdYlBu")),
-         mar = c(5, 4, 4, 2) + 0.1,
-         tl.col = "black",
-         #tl.pos = 'td',
-         tl.cex = 0.7,
-         diag = T, na.label = " ",
-         title = "Cluster Label Color by Annotation")
-
-### dotplot
+### prepare data for dot plot
 glist_df <- read.xlsx(paste0(dir_out, "cluster_annotation.xlsx"), sheet = "genes_processed")
 glist <- glist_df$Gene
+
 ct_mtx = so@assays[['RNA']]@data[glist,] %>% as.matrix()
 pct_0 = sapply(colnames(dist_mat_transformed), function(x){
   rowMeans(ct_mtx[, so$tissue_cluster == x] == 0)
@@ -95,22 +84,8 @@ ave_expr = sapply(colnames(dist_mat_transformed), function(x){
 dotplot_df <- data.frame(expr = as.numeric(ave_expr), pct = 1 - as.numeric(pct_0), 
          tissue_cluster = rep(colnames(dist_mat_transformed), each = length(glist)) %>% factor(., levels = rev(colnames(dist_mat_transformed))),
          gene = rep(glist, length(colnames(dist_mat_transformed))) %>% factor(., levels = unique(glist)))
-ggplot(dotplot_df) +
-  geom_point(aes(x = gene, y = tissue_cluster, size = pct, color = expr)) +
-  labs(x = "", y = "") +
-  scale_colour_gradient(low = "lightgrey", high = "blue") +
-  scale_size(range = c(0, 6), trans = scales::trans_new("square", function(x) x^2, "sqrt")) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-        # axis.text.y = element_text(color = rev(as.character(tissue_colors(tissue_vec)))),
-        legend.title = element_text(size = 8),
-        legend.box = "vertical", legend.position="right", legend.key.size = unit(.13, "in")) +
-  guides(color = guide_colorbar(title = "Scaled Average\nExpression", title.vjust = .8),
-         size = guide_legend(title = "Percent Cell\nDetected", title.vjust = .5, ))
-dev.off()
 
-
-## save data for future use
+## save data for plotting
 saveRDS(list("cluster_profile" = cluster_profile, 
              "cosine_sim" = cosine_sim, 
              "hierarchical_clustering" = hc, 
